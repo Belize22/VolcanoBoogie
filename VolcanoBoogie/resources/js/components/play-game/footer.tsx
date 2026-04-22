e04d528b1d82da7e3f890fa0d6dafccb1a34ce54
Peter Irshad <peter.irshad@gmail.com>
2026-04-22 18:10:41 -0400

    Color Adjustment - Self-explanatory.

--- VolcanoBoogie/resources/js/components/play-game/footer.tsx
+++ VolcanoBoogie/resources/js/components/play-game/footer.tsx
@@ -4,7 +4,7 @@ type Props = {
 export default function Sidebar({
 }: Props) {
     return (
-        <div className="fixed inset-x-0 bottom-0 right-0 h-1/10 bg-stone-800 border-l shadow-lg">
+        <div className="fixed inset-x-0 bottom-0 right-0 h-1/10 bg-stone-700 border-l shadow-lg">
             Footer Sidebar Content
         </div>
     );
--- VolcanoBoogie/resources/js/components/play-game/sidebar.tsx
+++ VolcanoBoogie/resources/js/components/play-game/sidebar.tsx
@@ -4,7 +4,7 @@ type Props = {
 export default function Sidebar({
 }: Props) {
     return (
-        <div className="fixed inset-y-0 right-0 w-2/10 bg-stone-600 border-l shadow-lg">
+        <div className="fixed inset-y-0 right-0 w-2/10 bg-stone-900 border-l shadow-lg">
             Right Sidebar Content
         </div>
     );
