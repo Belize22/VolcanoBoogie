620176425374c52ee05fd99f3a6d92fe613c835d
Peter Irshad <peter.irshad@gmail.com>
2026-05-07 15:37:54 -0400

    Transmit coordinate as object rather than 2 ints to front-end - Use accessor to transform the ints to the object and have it as a new field.

---
+++ VolcanoBoogie/app/Classes/Coordinate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Classes;
+
+class Coordinate {
+    public function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+}
--- VolcanoBoogie/app/Models/PlacedSubtile.php
+++ VolcanoBoogie/app/Models/PlacedSubtile.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use App\Classes\Coordinate;
 use App\Models\PlacedTile;
 use App\Enums\PathType;
 use App\Enums\Rotation;
@@ -13,17 +16,28 @@ use App\Enums\Property;
 #[Fillable(['placed_tile_id', 'x_coordinate', 'y_coordinate', 'path_type', 'rotation', 'property', 'is_neutralized'])]
 class PlacedSubtile extends Model
 {
+    protected $appends = ['coordinate'];
+    protected $hidden = ['x_coordinate', 'y_coordinate'];
+
     public function placedTile(): BelongsTo
     {
         return $this->belongsTo(PlacedTile::class);
+    }
+
+    protected function coordinate(): Attribute
+    {
+        return Attribute::make(
+            get: fn(mixed $value, array $attributes) => new Coordinate(
+                $attributes['x_coordinate'],
+                $attributes['y_coordinate'],
+            ),
+        );
     }
 
     protected function casts(): array
     {
         return [
             'placed_tile_id' => 'integer',
-            'x_coordinate' => 'integer',
-            'y_coordinate' => 'integer',
             'path_type' => PathType::class,
             'rotation' => Rotation::class,
             'property' => Property::class,
