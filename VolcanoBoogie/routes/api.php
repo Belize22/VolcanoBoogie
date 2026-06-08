f59f74afc79b6729fcbb6db4518a4d08c530cffd
Peter Irshad <peter.irshad@gmail.com>
2026-06-08 19:28:39 -0400

    Separate Game and Tile Placement Logic - Ensure that controller classes don't get too bloated (i.e. doesn't rely on one controller for all game functionality).

--- VolcanoBoogie/routes/api.php
+++ VolcanoBoogie/routes/api.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GameController;
+use App\Http\Controllers\TileController;
 
 
-Route::post('/place-tile', [GameController::class, 'placeTile'])->name('place-tile');
-Route::post('/confirm-tile-rotation', [GameController::class, 'confirmTileRotation'])->name('confirm-tile-rotation');
+Route::post('/place-tile', [TileController::class, 'placeTile'])->name('place-tile');
+Route::post('/confirm-tile-rotation', [TileController::class, 'confirmTileRotation'])->name('confirm-tile-rotation');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
